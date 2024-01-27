9f6ff5833502b19932488a95b9cd8ec7d2fff3ae
Issabel04 <isabellacero17@gmail.com>
2024-01-26 18:52:39 -0600

    Se creo el archivo README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+# Bienvenido a mi proyecto :D
+
+En éste repositorio almacenaremos el código necesario para ejecutar, visualizar y jugar nuestro videojuego.
+
+### Consideraciones
+
+Para ejecutar éste proyecto tenemos que instalar la última versión de python (3.12). Posteriormente, tenemos que instalar algunas librerias.
+
+### Librerias
+
+- pygame
+- numpy
+
+---
+
+Este proyecto sigue en desarrollo y es de código abierto, cualquier contribución se pueden contactar con: isabellacero17@gmail.com
---
+++ code/main.py
@@ -0,0 +1,28 @@
+import pygame, sys # Importamos pygame y sys
+
+# Creamos una clase la cual contendrá nuestro juego
+class Game: 
+    
+# Creamos un método constructivo para inicializar nuestros parámetros    
+    def __init__(self):
+        pygame.init() # Inicializamos pygame
+        self.screen = pygame.display.set_mode((128,128))  # Le decimos a pygame que tendrá una ventana de 128x128
+        self.clock = pygame.time.Clock() # Creamos un reloj interno
+
+    def ejecutar(self):
+        while True:
+    # Este ciclo recorre cada pulsación que haga el usuario
+            for evento in pygame.event.get():
+        # Si la pulsación coincide con el evento QUIT
+                if evento.type == pygame.QUIT:
+        # Le decimos que finalice        
+                    pygame.quit()
+        # Cerramos la ventaja que nos genere        
+                    sys.exit()
+
+            dt = self.clock.tick() / 1000
+            pygame.display.update()
+# Se crea una instancia de Game
+game = Game()
+# Ejecutamos el método "ejecutar"    
+game.ejecutar()
