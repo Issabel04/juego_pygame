809cd3c60b916990b7b2ec4ce5ae4e146b77bdbc
Issabel04 <isabellacero17@gmail.com>
2024-02-15 12:14:02 -0600

    Creamos la variable ruta_completa

--- code/support.py
+++ code/support.py
@@ -1,4 +1,5 @@
 from os import walk
+import pygame
 
 
 def import_folder(ruta_completa):
@@ -6,6 +7,12 @@ def import_folder(ruta_completa):
     lista_de_superficies = []
 
     for carpeta in walk(ruta_completa): 
-        print(carpeta)
-
+        for image in carpeta[-1]:   
+            if ':Zone.Identifier' in image:
+                continue
+            else:
+                ruta_entera = ruta_completa + '/' + image
+                superficie_imagen = pygame.image.load(ruta_entera).convert_alpha()
+                lista_de_superficies.append(superficie_imagen)
+    
     return lista_de_superficies
