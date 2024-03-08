561b5c372ee06da23af0f4991795f96c9a3b3afb
Issabel04 <isabellacero17@gmail.com>
2024-03-08 12:24:07 -0600

    Creamos un diccionario LAYER

--- code/settings.py
+++ code/settings.py
@@ -1,5 +1,6 @@
 VENTANA_LARGO = 700
 VENTANA_ANCHO = 700
+TILE_SIZE = 64
 
 NOMBRE_VIDEOJUEGO = 'Farlands'
 
@@ -7,4 +8,13 @@ POSICION_OVERLAY = {
     'herramienta': (40, VENTANA_LARGO - 15),
     'semilla': (70, VENTANA_LARGO - 5)
 
+}
+
+LAYER = {
+    'water' : 0,
+    'ground' : 1,
+    'house bottom' : 5,
+    'main' : 7,
+    'house top' : 8
+    
 }
